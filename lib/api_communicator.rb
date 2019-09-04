34d9e0468a8fa1e1f4418460aa05032a58660e94
Karina Macancela <karymacancela@gmail.com>
2019-09-04 11:50:06 -0400

    Cleaned up code

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,49 +3,38 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
+  # make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  character_hash = nil
 
+  while !character_hash
+    # iterate over the response hash to find the collection of `films` for the given `character`
+    # below will return nil or a hash for character_hash
+    character_hash = response_hash["results"].find do |character|
+      character["name"] == character_name
+    end
 
+    # collect those film API urls, make a web request to each URL to get the info
+    #  for that film
+    # return value of this method should be collection of info about each film.
+    #  i.e. an array of hashes in which each hash reps a given film
+    if character_hash
+      films = character_hash["films"]
+      films_hash =[]
+      films.each do |film|
+        films_hash <<  JSON.parse(RestClient.get(film))
+      end
 
-
-  # The below is the part that does not work
-
-  # puts response_hash
-  # (2...9).each do |page|
-  #   page_hash = JSON.parse(RestClient.get("http://www.swapi.co/api/people/?page=#{page}"))
-  #   puts page
-  #   response_hash["results"] << page_hash["results"]
-  #   # puts response_hash
-  # end
-  # puts response_hash["results"]
-
-
-
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-
-  character_hash = response_hash["results"].find do |character|
-    character["name"] == character_name
+      return films_hash
+    elsif response_hash["next"]
+      response_hash = JSON.parse(RestClient.get(response_hash["next"]))
+    else
+      films_hash = nil
+       return
+    end
   end
 
-  films = character_hash["films"]
-
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-
-  films_hash =[]
-  films.each do |film|
-    films_hash <<  JSON.parse(RestClient.get(film))
-  end
-
-  print_movies(films_hash)
-
-  binding.pry
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
@@ -68,7 +57,15 @@ end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  if films
+    print_movies(films)
+  else
+    puts "*" * 25
+    puts ""
+    puts "No character found!"
+  end
+  binding.pry
+  0
 end
 
 ## BONUS
